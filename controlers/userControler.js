3608e09dd760132bc2f7db4797ad432cc60c2393
Mrs-ID <162367920+Mrs-ID@users.noreply.github.com>
2024-03-11 20:28:27 +0200

    Update userControler.js

--- controlers/userControler.js
+++ controlers/userControler.js
@@ -5,14 +5,14 @@ const getAllUsers = async (req, res) => {
     res.json(users)
 }
 
-const getUsersById = async (req, res) => {
+const getUserById = async (req, res) => {
     const { id } = req.params
     const users = await Users.findById(id).lean()
     if (!users) return res.status(404).json("No users found")
     res.json(users)
 }
 
-const createUsers = async (req, res) => {
+const createUser = async (req, res) => {
     const { name,username,email,address,phone } = req.body
     if (!name || !phone) return res.status(400).json("Name and phone is required fields")
     const users = await Users.create({name,username,email,address,phone})
@@ -21,7 +21,7 @@ const createUsers = async (req, res) => {
     res.json(users)
 }
 
-const updateUsers = async (req, res) => {
+const updateUser = async (req, res) => {
     const { id,name,username,email,address,phone } = req.body
     if (!id) {
         return res.status(404).send("ID is required")
@@ -35,7 +35,7 @@ const updateUsers = async (req, res) => {
     const newUsers = await users.save()
     res.json(`update users ${id} success`)
 }
-const deleteUsers = async (req, res) => {
+const deleteUser = async (req, res) => {
     const { id } = req.body
     if (!id) {
         return res.status(404).send("id is required")
