124405cb367620de5dd3d2ca8e82795943830dc9
Mrs-ID <i6725155@gmail.com>
2024-03-11 20:55:44 +0200

    add router

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express")
 
 const app = express()
 
+
 const PORT = process.env.PORT || 5555
+
+app.use("/api/users", require("./routes/userRoutes"))
 
 app.get("/", (req, res) => {
     res.json("test")
